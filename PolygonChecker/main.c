8499de25b68a11031b1ba5870b3a5b42a964e532
taiyo <tsuzuki7660@conestogac.on.ca>
2022-11-10 16:21:46 -0500

    comments added, as well as some small additions.

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -4,6 +4,15 @@
 #include "main.h"
 #include "triangleSolver.h"
 
+//Taiyo Suzuki, Jack Lombardi, Logan Haiser
+//December 2022
+//Write a program with 2 functionalities:
+//1. Accept 3 user input numbers, check if they make a triangle, and if they do, display what type of triangle it is 
+//and calculate and display the inside angles between the sides.
+//2. Accept 4 user input points as x/y pairs (x, y), generate lines between the points and determine if they form a rectangle.
+//Report the perimiter of the shape, and if it is a rectangle, report the area. To create the rectangle, 
+//determine which corner each point is by calculation, then connect each corner in order. This eliminates the possibility of lines crossing.
+
 int side = 0;
 
 int main() {
@@ -15,7 +24,10 @@ int main() {
 
 		switch (shapeChoice)
 		{
-		case 1:
+		case 0:																								//case 0: exit (written as case 0)
+			continueProgram = false;
+			break;
+		case 1:																								//case 1: triangle checking
 			printf_s("Triangle selected.\n");
 			int triangleSides[3] = { 0, 0, 0 };
 			int* triangleSidesPtr = getTriangleSides(triangleSides);
@@ -23,18 +35,18 @@ int main() {
 			char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
 			printf_s("%s\n", result);
 			break;
-		case 0:
-			continueProgram = false;
+		case 2:																								//case 2: rectangle checking
+			printf_s("Rectangle selected.\n");
 			break;
 		default:
-			printf_s("Invalid value entered.\n");
+			printf_s("Invalid value entered.\n");															//default case (number input other than 0-2)
 			break;
 		}
 	}
 	return 0;
 }
 
-void printWelcome() {
+void printWelcome() {																						//print welcome header
 	printf_s("\n");
 	printf_s(" **********************\n");
 	printf_s("**     Welcome to     **\n");
@@ -42,8 +54,10 @@ void printWelcome() {
 	printf_s(" **********************\n");
 }
 
-int printShapeMenu() {
+int printShapeMenu() {																						//print function menu
+	printf_s("\n");
 	printf_s("1. Triangle\n");
+	printf_s("2. Rectangle\n");
 	printf_s("0. Exit\n");
 
 	int shapeChoice;
@@ -54,7 +68,7 @@ int printShapeMenu() {
 	return shapeChoice;
 }
 
-int* getTriangleSides(int* triangleSides) {
+int* getTriangleSides(int* triangleSides) {																	//get triangle input
 	printf_s("Enter the three sides of the triangle: ");
 	for (int i = 0; i < 3; i++)
 	{
--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -3,20 +3,35 @@
 
 #include "triangleSolver.h"
 
+//Taiyo Suzuki, Jack Lombardi, Logan Haiser
+//December 2022
+//Write a program with 2 functionalities:
+//1. Accept 3 user input numbers, check if they make a triangle, and if they do, display what type of triangle it is 
+//and calculate and display the inside angles between the sides.
+//2. Accept 4 user input points as x/y pairs (x, y), generate lines between the points and determine if they form a rectangle.
+//Report the perimiter of the shape, and if it is a rectangle, report the area. To create the rectangle, 
+//determine which corner each point is by calculation, then connect each corner in order. This eliminates the possibility of lines crossing.
+
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+															//a triangle is valid if it satisfies three conditions: (a + b > c) (a + c > b) (b + c > a)
+
+	if (side1 <= 0 || side2 <= 0 || side3 <= 0)				//triangle sides can't be less than or equal to zero
+	{			
 		result = "Not a triangle";
 	}
-	else if (side1 == side2 && side1 == side3) {
+	else if (side1 == side2 && side1 == side3)				//all equal sides == equilateral triangle
+	{			
 		result = "Equilateral triangle";
 	}
 	else if ((side1 == side2 && side1 != side3) || 
-		(side1 == side3 && side1 != side2))
+		(side1 == side3 && side1 != side2) || 
+		(side2 == side3 && side2 != side1))					//2 sides the same == isosceles triangle
 	{
 		result = "Isosceles triangle";
 	}
-	else {
+	else													//all other triangles are scalene
+	{													
 		result = "Scalene triangle";
 	}
 
